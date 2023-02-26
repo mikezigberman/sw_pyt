6bc6372ae737312cd82b7ca857968d26e4939c5b
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-26 18:51:35 +0100

    find job service(creating basic project applications): creating a new model (programming language)

--- job_scraping_service/src/scraping/models.py
+++ job_scraping_service/src/scraping/models.py
@@ -3,12 +3,28 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=50,
-                            verbose_name='City name')
-    slug = models.CharField(max_length=50, blank=True)
+                            verbose_name='City name',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True,
+                            unique=True)
     
     class Meta:
         verbose_name='City name'
         verbose_name_plural='City names'
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Programming language',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Programming language'
+        verbose_name_plural = 'Programming languages'
 
     def __str__(self):
         return self.name
