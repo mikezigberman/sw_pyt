513c72948a2b6fa6e0d37386af63fcab87224ebc
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-26 20:51:37 +0100

    find job service(creating basic project applications): added model for saving vacancies

--- job_scraping_service/src/scraping/models.py
+++ job_scraping_service/src/scraping/models.py
@@ -26,3 +26,12 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+class Vacancy(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=250, verbose_name='')
+    company = models.CharField(max_length=250, verbose_name='')
+    description = models.TextField(verbose_name='')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='')
