d3cc3de222e592ab425df19195ccc78a640170ed
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-26 18:32:11 +0100

    find job service(creating basic project applications): work with migrations

--- job_scraping_service/src/scraping/models.py
+++ job_scraping_service/src/scraping/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,
+                            verbose_name='City name')
     slug = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        verbose_name='City name'
+        verbose_name_plural='City names'
+
+    def __str__(self):
+        return self.name
