b911a1d7b4fc435c0496ca19bde867d0aa16de55
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-28 17:32:09 +0100

    find job service(working with forms): passing a slug through a form

--- job_scraping_service/src/scraping/forms.py
+++ job_scraping_service/src/scraping/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 from scraping.models import City, Language
 
+
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.all())
-    language = forms.ModelChoiceField(queryset=Language.objects.all())
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(), to_field_name="slug", required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='City'
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(), to_field_name="slug", required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Speciality'
+    )
